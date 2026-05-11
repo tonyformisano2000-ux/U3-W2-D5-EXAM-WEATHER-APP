a713ae3b9bf82cf3a4d1a20d1f0baadb0543d683
Antonio <tony.formisano.2000@gmail.com>
2026-05-11 14:06:32 +0200

    uodate

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,12 +1,12 @@
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import Location from "./Location";
 import WeatherTable from "./WeatherTable";
 import { useState, useEffect } from "react";
 
 const Detail = () => {
-  const [results, setResults] = useState();
-
+  const [results, setResults] = useState(null);
   const params = useParams();
+
   useEffect(() => {
     fetch(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${params.lat}&lon=${params.lon}&appid=d85c199fc44d1cb4c6a19d30a6f7a971`,
@@ -24,7 +24,11 @@ const Detail = () => {
       });
     console.log("dati di Roma ", results);
   }, [params.lat, params.lon]);
-
+  if (!results || !results.list) {
+    return (
+      <p className="text-center mt-5 text-white">Caricamento previsioni...</p>
+    );
+  }
   return (
     <>
       <Location lat={params.lat} lon={params.lon}></Location>
